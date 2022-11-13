69f6e8a313e8b223b63fb0064e28d9b500522951
DgL1797 <digiacomoluca1797@gmail.com>
2022-11-13 19:24:11 +0100

    important change

--- src/main/java/unipi/mirs/SearchEngine.java
+++ src/main/java/unipi/mirs/SearchEngine.java
@@ -17,87 +17,90 @@ import unipi.mirs.utilities.Constants;
 
 public class SearchEngine {
 
-  private static String inputFile = Paths.get(Constants.INPUT_DIR.toString(), "collection.tsv").toString();
-  private static boolean readCompressed = false;
-  private static final Scanner stdin = new Scanner(System.in);
+    private static String inputFile = Paths.get(Constants.INPUT_DIR.toString(), "collection.tsv").toString();
+    private static boolean readCompressed = false;
+    private static final Scanner stdin = new Scanner(System.in);
 
-  /**
-   * Allows the user to select a new file as collection of the search engine, the collection has to be in .tsv format
-   * with 'docno\tdocbody\n' format, the tsv file can also be passed with gzip commpression and the function will
-   * automatically select if the file needs to be parsed with decompressor or not It will modify inputFile and
-   * readCompressed class' static parameters in-place.
-   * 
-   * @throws IOException
-   */
-  private static void changeInputFile() throws IOException {
-    // while inserted file not found keep saying: "Error ${filename} not in input folder"
-    File inputDir = new File(Constants.INPUT_DIR.toString());
+    /**
+     * Allows the user to select a new file as collection of the search engine, the collection has to be in .tsv format
+     * with 'docno\tdocbody\n' format, the tsv file can also be passed with gzip commpression and the function will
+     * automatically select if the file needs to be parsed with decompressor or not It will modify inputFile and
+     * readCompressed class' static parameters in-place.
+     * 
+     * @throws IOException
+     */
+    private static void changeInputFile() throws IOException {
+        // while inserted file not found keep saying: "Error ${filename} not in input folder"
+        File inputDir = new File(Constants.INPUT_DIR.toString());
 
-    String[] files = Arrays.asList(inputDir.listFiles()).stream().filter((f) -> f.isFile())
-        .filter((f) -> f.toString().matches(".*\\.gz$") || f.toString().matches(".*\\.tsv$")).map(f -> f.toString())
-        .toArray(String[]::new);
+        String[] files = Arrays.asList(inputDir.listFiles()).stream().filter((f) -> f.isFile())
+                .filter((f) -> f.toString().matches(".*\\.gz$") || f.toString().matches(".*\\.tsv$"))
+                .map(f -> f.toString()).toArray(String[]::new);
 
-    if (Arrays.asList(files).size() == 0) {
-      System.out
-          .println(ConsoleUX.FG_RED + ConsoleUX.BOLD + "No files found, make sure to import a .tsv or .gz file inside "
-              + Constants.INPUT_DIR + " folder" + ConsoleUX.RESET);
-      inputFile = "";
-    }
-    Menu filesMenu = new Menu(stdin, files);
-    inputFile = files[filesMenu.printMenu()];
-    if (inputFile.matches(".*\\.gz$"))
-      readCompressed = true;
-  }
-
-  private static void buildIndex() {
-    if (readCompressed) {
-
-    } else {
-      System.out.println(ConsoleUX.CLS + ConsoleUX.BOLD + ConsoleUX.FG_BLUE + "Processing File..." + ConsoleUX.RESET);
-      try (BufferedReader inreader = Files.newBufferedReader(Path.of(inputFile), StandardCharsets.UTF_8)) {
-        String document;
-        IndexBuilder ib = new IndexBuilder(5000, stdin);
-        while ((document = inreader.readLine()) != null) {
-          // addDocument will automatically save as it reaches its limit and reset itself
-          if (!ib.addDocument(document)) {
-            return;
-          }
+        if (Arrays.asList(files).size() == 0) {
+            System.out.println(
+                    ConsoleUX.FG_RED + ConsoleUX.BOLD + "No files found, make sure to import a .tsv or .gz file inside "
+                            + Constants.INPUT_DIR + " folder" + ConsoleUX.RESET);
+            inputFile = "";
         }
-        // ib.write(final=true); will write in files the data structures
-      } catch (IOException e) {
-        System.out.println(ConsoleUX.FG_RED + ConsoleUX.BOLD + "Unable to initialize buffer for " + inputFile + ":\n"
-            + e.getMessage() + ConsoleUX.RESET);
-        ConsoleUX.pause(false, stdin);
-      }
+        Menu filesMenu = new Menu(stdin, files);
+        inputFile = files[filesMenu.printMenu()];
+        if (inputFile.matches(".*\\.gz$"))
+            readCompressed = true;
     }
-    // open inputfile using BufferedReader or ZipBufferedReader based on compressedRead
-    // if compressed file, decompress it and move it to a normal Buffered stream
-    // read up to 5000 lines
-    //   create IndexBuilder instance
-    //   normilize docbodies
-    //   update IndexBuilder instance
-    //   write IndexBuilder to file
-    //   reset IndexBuilder instance
-  }
 
-  public static void main(String[] args) throws IOException {
-    // Save index in a file, compressed index in another one to avoid re-building index every time
-    Menu menu = new Menu(stdin, "Change Input File", "Build Index", "Compress Inverted Index", "Search", "Exit");
-    int opt = 0;
-    while ((opt = menu
-        .printMenu(ConsoleUX.FG_BLUE + ConsoleUX.BOLD + "Selected File: " + inputFile + ConsoleUX.RESET)) != 4) {
-      if (opt == 0) {
-        changeInputFile();
-      } else if (opt == 1) {
-        buildIndex();
-      } else if (opt == 2) {
+    private static void buildIndex() {
+        if (readCompressed) {
 
-      } else if (opt == 3) {
-
-      }
+        } else {
+            System.out.println(
+                    ConsoleUX.CLS + ConsoleUX.BOLD + ConsoleUX.FG_BLUE + "Processing File..." + ConsoleUX.RESET);
+            try (BufferedReader inreader = Files.newBufferedReader(Path.of(inputFile), StandardCharsets.UTF_8)) {
+                String document;
+                IndexBuilder ib = new IndexBuilder(5000, stdin);
+                while ((document = inreader.readLine()) != null) {
+                    // addDocument will automatically save as it reaches its limit and reset itself
+                    if (!ib.addDocument(document)) {
+                        return;
+                    }
+                }
+                // ib.write(final=true); will write in files the data structures
+            } catch (IOException e) {
+                System.out.println(ConsoleUX.FG_RED + ConsoleUX.BOLD + "Unable to initialize buffer for " + inputFile
+                        + ":\n" + e.getMessage() + ConsoleUX.RESET);
+                ConsoleUX.pause(false, stdin);
+            }
+        }
+        // open inputfile using BufferedReader or ZipBufferedReader based on compressedRead
+        // if compressed file, decompress it and move it to a normal Buffered stream
+        // read up to 5000 lines
+        //   create IndexBuilder instance
+        //   normilize docbodies
+        //   update IndexBuilder instance
+        //   write IndexBuilder to file
+        //   reset IndexBuilder instance
     }
-    System.out.println(ConsoleUX.CLS + ConsoleUX.FG_YELLOW + ConsoleUX.BOLD + "UOT MATTEMATTICALLI DU UI DU!");
-    System.out.println("DU UI LAIK TU UEIST TIME? 0,0001 milliseconds to print" + ConsoleUX.RESET);
-    System.exit(0);
-  }
+
+    public static void main(String[] args) throws IOException {
+        // Save index in a file, compressed index in another one to avoid re-building index every time
+        Menu menu = new Menu(stdin, "Change Input File", "Build Index", "Compress Inverted Index", "Search", "Exit");
+        int opt = 0;
+        while ((opt = menu.printMenu(
+                ConsoleUX.FG_BLUE + ConsoleUX.BOLD + "Selected File: " + inputFile + ConsoleUX.RESET)) != 4) {
+            if (opt == 0) {
+                changeInputFile();
+            } else if (opt == 1) {
+                buildIndex();
+            } else if (opt == 2) {
+
+            } else if (opt == 3) {
+
+            }
+        }
+        System.out.println(ConsoleUX.CLS + ConsoleUX.FG_YELLOW + ConsoleUX.BOLD
+                + "UI NID TU EGZIT, UOT MATTEMMATTICCALLI DU UI DU? UI COMMPLITLLY FORMÀT D COMPIUTTER, BICOS:");
+        System.out.println(
+                "DU UI LAIK TU UEIST SPEIS? DU UI LAIK TU UEIST TIME? 0,0001 milliseconds to exit" + ConsoleUX.RESET);
+        System.exit(0);
+    }
 }
