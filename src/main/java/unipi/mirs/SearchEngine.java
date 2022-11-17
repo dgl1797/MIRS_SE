351b8ecfd7f288b548a1fa7b087238cd24becd2e
DgL1797 <digiacomoluca1797@gmail.com>
2022-11-17 10:05:15 +0100

    .

--- src/main/java/unipi/mirs/SearchEngine.java
+++ src/main/java/unipi/mirs/SearchEngine.java
@@ -8,8 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.BitSet;
-import java.util.HashMap;
 import java.util.Scanner;
 
 import unipi.mirs.components.IndexBuilder;
