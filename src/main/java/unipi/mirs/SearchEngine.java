eeea2b00c111e5e21d1392345d2986978a783bc6
DgL1797 <digiacomoluca1797@gmail.com>
2022-11-27 15:24:28 +0100

    removed menu

--- src/main/java/unipi/mirs/SearchEngine.java
+++ src/main/java/unipi/mirs/SearchEngine.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 import unipi.mirs.components.DocTable;
 import unipi.mirs.components.Vocabulary;
 import unipi.mirs.graphics.ConsoleUX;
-import unipi.mirs.graphics.Menu;
 
 /**
  * @formatter:off
@@ -51,54 +50,45 @@ public class SearchEngine {
   public static void main(String[] args) throws IOException {
     try {
       // setup
-      Menu queryMenu = new Menu(stdin, "Change Mode", "Start Search", "Exit");
       ConsoleUX.DebugLog(ConsoleUX.CLS + "Loading...");
       Vocabulary lexicon = new Vocabulary();
       lexicon.loadVocabulary();
       DocTable doctable = new DocTable();
       doctable.loadDocTable();
-      int choice;
-      // menu
-      while ((choice = queryMenu.printMenu(String.format(ConsoleUX.FG_BLUE + ConsoleUX.BOLD + "Current query mode: %s",
-          (isConjunctive ? "Conjunctive " : "Disjunctive ")))) != queryMenu.exitMenuOption()) {
-        if (choice == 0) {
-          isConjunctive = !isConjunctive;
-        } else if (choice == 1) {
-          String[] top20 = new String[20];
-          ConsoleUX.SuccessLog(ConsoleUX.CLS + "/help/ - prints the guide for all possible commands");
-          ConsoleUX.SuccessLog("/mode/ - changes query mode(conjunctive - disjunctive)");
-          ConsoleUX.SuccessLog("/exit/ - goes back to main menu");
-          // interactive querying
-          while (true) {
-            ConsoleUX.SuccessLog("Search", "");
-            ConsoleUX.DebugLog("[" + (isConjunctive ? "c" : "d") + "]", "");
-            ConsoleUX.SuccessLog(": ", "");
-            String query = stdin.nextLine();
-            // query system commands
-            if (query.matches("^\\/.+\\/$")) {
-              if (handleCommand(query.replaceAll("\\/", ""))) {
-                // termination if user enters /exit/ in the search field
-                break;
-              }
-              System.out.print(ConsoleUX.CLS);
-              continue;
-            }
-            // query start
-            long before = System.currentTimeMillis();
-
-            // top20 = search(query);
-
-            long delta = System.currentTimeMillis() - before;
-            // query end
-
-            // documents printing
-            ConsoleUX.DebugLog("Parsed " + doctable.ndocs + " documents in " + delta + "ms:");
-            for (int i = 0; i < top20.length; i++) {
-              ConsoleUX.SuccessLog((i + 1) + ".\t" + top20[i]);
-            }
-            ConsoleUX.pause(true, stdin);
+      // guide
+      String[] top20 = new String[20];
+      ConsoleUX.SuccessLog(ConsoleUX.CLS + "Commands:\n/help/ - prints the guide for all possible commands");
+      ConsoleUX.SuccessLog("/mode/ - changes query mode(conjunctive - disjunctive)");
+      ConsoleUX.SuccessLog("/exit/ - quits the search engine");
+      // interactive querying
+      while (true) {
+        ConsoleUX.SuccessLog("Search", "");
+        ConsoleUX.DebugLog("[" + (isConjunctive ? "c" : "d") + "]", "");
+        ConsoleUX.SuccessLog(": ", "");
+        String query = stdin.nextLine();
+        // query system commands
+        if (query.matches("^\\/(help|mode|exit)\\/$")) {
+          if (handleCommand(query.replaceAll("\\/", ""))) {
+            // termination if user enters /exit/ in the search field
+            break;
           }
+          System.out.print(ConsoleUX.CLS);
+          continue;
         }
+        // query start
+        long before = System.currentTimeMillis();
+
+        // top20 = search(query);
+
+        long delta = System.currentTimeMillis() - before;
+        // query end
+
+        // documents printing
+        ConsoleUX.DebugLog("Parsed " + doctable.ndocs + " documents in " + delta + "ms:");
+        for (int i = 0; i < top20.length; i++) {
+          ConsoleUX.SuccessLog((i + 1) + ".\t" + top20[i]);
+        }
+        ConsoleUX.pause(true, stdin);
       }
     } catch (IOException e) {
       ConsoleUX.ErrorLog("Search failed:");
