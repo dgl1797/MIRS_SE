fe5ae069e0d3e1b8a4c67cafb08af67bdabbf041
Paulfinex <sentius96@gmail.com>
2022-11-29 16:21:44 +0100

    Create DocTable.java

---
+++ src/main/java/unipi/mirs/components/DocTable.java
@@ -0,0 +1,48 @@
+package unipi.mirs.components;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+import unipi.mirs.utilities.Constants;
+
+public class DocTable {
+  //[0] is the docno , [1] the doclen
+  public HashMap<Integer, Object[]> doctable = new HashMap<>();
+  public int ndocs = 0;
+  public double avgDocLen = 0;
+
+  public DocTable() {}
+
+  private Object[] getComponents(String line) {
+    String[] parts = line.split("-");
+    return new Object[] { parts[0], Integer.parseInt(parts[1]) };
+  }
+
+  public void loadDocTable() throws IOException {
+    Path doctablePath = Paths.get(Constants.OUTPUT_DIR.toString(), "doctable.dat");
+    File doctableFile = new File(doctablePath.toString());
+    if (!doctableFile.exists()) {
+      throw new IOException("Unable to retrieve the vocabulary from the index's file system");
+    }
+    BufferedReader dbr = Files.newBufferedReader(doctablePath, StandardCharsets.UTF_8);
+    String line;
+    while ((line = dbr.readLine()) != null) {
+      this.ndocs += 1;
+      String[] parts = line.split("\t");
+      int docid = Integer.parseInt(parts[0]);
+      avgDocLen += Integer.parseInt(parts[1].split("-")[1]);
+      if (doctable.containsKey(docid)) {
+        throw new IOException("Malformed Document table");
+      }
+      doctable.put(docid, getComponents(parts[1]));
+    }
+    avgDocLen /= ndocs;
+  }
+
+}
